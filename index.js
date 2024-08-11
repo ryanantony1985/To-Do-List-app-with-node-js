c360631e12e840688ebfed9e2ec11eee5b626404
ryanantony1985 <antonyryan1985@gmail.com>
2024-08-11 19:31:48 +0530

    test deployement

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const TodoTask = require("./models/TodoTask");
 dotenv.config();
 
 // Connect to MongoDB
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+    });
+ 
+
+
 
 // Set up EJS as the view engine
 app.set('view engine', 'ejs');
